9562f545a8580c81730ea4b0edb5bfd562beb025
augustobrandi2024 <augusto.brandi@escola.pr.gov.br>
2026-06-03 12:17:33 -0300

    augusto

--- main.js
+++ main.js
@@ -10,5 +10,10 @@ for(let i=0;i <botoes.length;i++){
         }
 
         botoes[i].classList.add("ativo");
-    }
+        textos{i}.classList.add("ativo")
+    };
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-10-10T00:00:00");
+let tempoAtual = new Date ();
